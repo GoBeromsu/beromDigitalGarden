f4ef863832be892332e7a6179f922c23d487d129
고범수 <gobeumsu@gmail.com>
2024-03-21 12:10:59 +0900

    Update files

--- src/site/_data/eleventyComputed.js
+++ src/site/_data/eleventyComputed.js
@@ -3,10 +3,7 @@ const { getFileTree } = require("../../helpers/filetreeUtils");
 const { userComputed } = require("../../helpers/userUtils");
 
 module.exports = {
-  // getGraph 함수를 사용하여 그래프를 생성합니다.
   graph: (data) => getGraph(data),
-  // getFileTree 함수를 사용하여 파일 트리를 생성합니다.
   filetree: (data) => getFileTree(data),
-  // userComputed 함수를 사용하여 사용자 계산 값을 생성합니다.
-  userComputed: (data) => userComputed(data),
+  userComputed: (data) => userComputed(data)
 };
